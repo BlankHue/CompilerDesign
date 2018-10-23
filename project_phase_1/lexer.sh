9e659670e73a6e576699715d39bf3c9f6fc19d02
BlankhDevelopment <42626093+BlankhDevelopment@users.noreply.github.com>
2018-10-22 18:21:45 -0700

    Create lexer.sh

---
+++ project_phase_1/lexer.sh
@@ -0,0 +1,6 @@
+echo "Testing lexer with fibonacci.min"
+cat fibonacci.min | lexer
+echo "Testing lexer with mytest.min
+cat mytest.min | lexer
+echo "Testing lexer with primes.min
+cat primes.min | lexer
